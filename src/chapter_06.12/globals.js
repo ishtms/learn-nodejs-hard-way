374a22d02fadac0d4cca06730fd08c2980be1e87
Ishtmeet <ish.rissam@gmail.com>
2024-09-03 20:15:17 +0530

    updates chapter naming if the sub-chapters are more than 10 for maintaining lexicographical ordering on github and generated files

--- src/chapter_06.12/globals.js
+++
@@ -1,7 +0,0 @@
-/**
- * @typedef { 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD' | 'OPTIONS' | 'CONNECT' | 'TRACE' } HttpMethod
- */
-
-/**
- * @typedef {import("http").RequestListener} RequestHandler
- */
